0b72b34cdcb9e8fbed6bdf96089847fac766d65a
lauragarling <44353212+lauragarling@users.noreply.github.com>
2018-10-23 17:27:11 -0500

    Update MyLevels.java

--- MyLevels.java
+++ MyLevels.java
@@ -3,12 +3,14 @@ import java.util.Arrays;
 public class MyLevels {
 	
 	public static final char EMPTY_CHAR = ' ';
-	 	public static final char BOX_CHAR = '='; // Box character
-	    public static final char WALL_CHAR   = '#'; // Wall character
-	    public static final char WORKER_CHAR  = '@'; // Worker character
-	    public static final char GOAL_CHAR  = '.'; // Goal character
-	    public static final char BOX_GOAL_CHAR  = '*'; // Box on a goal character
-	    public static final char WORK_GOAL_CHAR  = '+'; // Worker on a goal character
+	public static final char BOX_CHAR = '='; // Box character
+	public static final char WALL_CHAR   = '#'; // Wall character
+	public static final char WORKER_CHAR  = '@'; // Worker character
+	public static final char GOAL_CHAR  = '.'; // Goal character
+	public static final char BOX_GOAL_CHAR  = '*'; // Box on a goal character
+	public static final char WORK_GOAL_CHAR  = '+'; // Worker on a goal character
+	
+	public static final long SEED            = 1234; //the random seed
 
 	public static final char[][][] LEVELS = {
 			{
@@ -68,8 +70,13 @@ public class MyLevels {
 			{10, 3, 10, 4, 10, 5}
 	};
 	
-	public static void main(String[] args)
-	{
+    public static final char UP_CHAR = '8';
+    public static final char DOWN_CHAR = '2';
+    public static final char LEFT_CHAR = '4';
+    public static final char RIGHT_CHAR = '6';
+    public static final char QUIT_CHAR = 'q';
+	//public static void main(String[] args)
+	//{
 		//System.out.print(Arrays.toString(LEVELS));
-	}
+	//}
 }
